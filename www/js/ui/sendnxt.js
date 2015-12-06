83adfc7a6978b397c2ffd0ecb5ab6292ea50f6f5
skynxt <skynxtd@gmail.com>
2015-12-06 12:29:12 +0530

    modifying config file to access network & others

--- www/js/ui/sendnxt.js
+++ www/js/ui/sendnxt.js
@@ -152,13 +152,8 @@ $scope.scanBarCode = function()
 	try {
 	cordova.plugins.barcodeScanner.scan(
 	  function (result) {
-		  alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
 		  if(result.cancelled == false && result.format == "QR_CODE")
 		  {
-			  alert(1)
 			  $scope.recipient_address.text = String(result.text);
 		  }
 	  }, 
